72fd8a46ac58e896bf06d2b21580cf787b21a668
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-09-24 17:05:19 +0100

    add cors access for prod url

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,2 +1,6 @@
-const allowedOrigin = ["http://127.0.0.1:4000", "http://localhost:5173"];
+const allowedOrigin = [
+  "http://127.0.0.1:4000",
+  "http://localhost:5173",
+  "https://tumblr-blog.vercel.app",
+];
 module.exports = allowedOrigin;
