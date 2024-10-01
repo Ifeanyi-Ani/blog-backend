3dec9c75a115fe48084e996596622b882f8abe44
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-10-01 09:24:22 +0100

    add more custom error to display meaningful message

--- controllers/errController.js
+++ controllers/errController.js
@@ -1,10 +1,77 @@
-module.exports = (err, _req, res, _next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const AppErr = require("../utils/appErr");
+const { Mongoose, default: mongoose } = require("mongoose");
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppErr(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppErr(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppErr(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppErr("Invalid token. Please log in again!", 401);
+const handleJWTExpiredError = () =>
+  new AppErr("Your token has expired! Please log in again.", 401);
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
 };
 
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or other unknown error: don't leak error details
+  else {
+    // 1) Log error
+    console.error("ERROR 💥", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+      err: err,
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  const nodeEnv = req.get("NODE_ENV") || process.env.NODE_ENV || "development";
+
+  if (nodeEnv === "development") {
+    sendErrorDev(err, res);
+  } else if (nodeEnv === "production") {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (err.name === "CastError") error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
+    if (err.name === "JsonWebTokenError") error = handleJWTError();
+    if (err.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+    sendErrorProd(error, res);
+  }
+};
